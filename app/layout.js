27c1cb016d3f671eab8f8745f67ca7d0adf46433
LoLoSenPai <loic.dlugosz@gmail.com>
2024-03-05 12:17:44 +0100

    Add metadata and icons to layout.js

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from 'next/font/google'
 import ThemeToggle from "./components/toggleTheme"
 import './components/toggleTheme.css'
 import './globals.css'
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,8 +14,18 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        {/* Favicon */}
+        <link rel="icon" href="/favicon.ico" />
+
+        {/* Apple Touch Icon */}
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+
+        {/* Manifest */}
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
       <body className={inter.className}>
-      <ThemeToggle />
+        <ThemeToggle />
         {children}</body>
     </html>
   )
