81cd2068a6a24c6b6a7a392ef350f97b7fe3776a
LoLoSenPai <loic.dlugosz@gmail.com>
2024-03-21 14:29:29 +0100

    Update metadata in layout.js and remove unused image files

--- app/layout.js
+++ app/layout.js
@@ -9,6 +9,17 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: 'Portfolio',
   description: 'Loic Dlugosz Portfolio.',
+  icon: '/favicon.ico',
+  apple: '/apple-touch-icon.png',
+  shortcut: '/apple-touch-icon.png',
+  openGraph: {
+    title: 'Portfolio',
+    description: 'Loic Dlugosz Portfolio.',
+    image: '/apple-touch-icon.png',
+    url: 'https://lololabs.vercel.app/',
+    type: 'website',
+    locale: 'en_US',
+  },
 }
 
 export default function RootLayout({ children }) {
